9a37237144556dc518c1fdbbc70c48d50f54328a
MichalMoudry <moudry.michal@live.com>
2020-10-20 11:07:34 +0200

    Added local storage sample

---
+++ Models/Email.cs
@@ -0,0 +1,9 @@
+namespace demo.Models
+{
+    public class Email
+    {
+        public string ID { get; set; }
+
+        public string JsonContent { get; set; }
+    }
+}
---
+++ Models/EmailContent.cs
@@ -0,0 +1,15 @@
+namespace demo.Models
+{
+    public class EmailContent
+    {
+        public string EmailID { get; set; }
+
+        public string Title { get; set; }
+        
+        public string To { get; set; }
+
+        public string From { get; set; }
+
+        public string Content { get; set; }
+    }
+}
